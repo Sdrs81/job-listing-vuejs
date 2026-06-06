72d47002c073a7b928622a412ae94fbc9800e157
Sdrs81 <serdardemirtas25@gmail.com>
2026-06-06 21:34:21 +0300

    Navbar, Hero and Cards have been added

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,15 @@
-<script setup></script>
+<script setup>
+import Navbar from "@/components/Navbar.vue";
+import Hero from "@/components/Hero.vue";
+import HomeCards from "./components/HomeCards.vue";
+import JobListings from "./components/JobListings.vue";
+</script>
 
 <template>
-  <h1>Vue Jobs</h1>
+  <Navbar />
+  <Hero />
+  <HomeCards />
+  <JobListings />
 </template>
 
 <style scoped></style>
